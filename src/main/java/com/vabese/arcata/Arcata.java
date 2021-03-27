45cd9566f3fa5d053968636de3920c9992950c5b
vabese <vbshekert@hotmail.com>
2021-03-27 02:19:41 +0100

    Update Arcata.java

--- src/main/java/com/vabese/arcata/Arcata.java
+++ src/main/java/com/vabese/arcata/Arcata.java
@@ -3,6 +3,9 @@ package com.vabese.arcata;
 import net.fabricmc.api.ModInitializer;
 
 public class Arcata implements ModInitializer{
+
+    public static final String MOD_ID = "arcata";
+
     @Override
     public void onInitialize() {
 
